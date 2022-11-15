04375d0bba26e319a836bd7d18a263aad9370a23
Marco Antônio <marcomartins06@rede.ulbra.br>
2022-11-15 19:50:51 -0300

    usando funções para verificar comandos + troca de match case para if e elif

--- main.py
+++ main.py
@@ -3,24 +3,70 @@ from valores_atualizados import get_valor_btc_atual, get_valor_dolar_atual
 import discord
 import os
 
+def saudacoes(mensagem):
+    lista_saudacoes = ['Oi', 'olá', 'bom dia', 'boa tarde', 'boa noite']
+    for palavra in lista_saudacoes:
+        if palavra in mensagem:
+            return True
+    return False
+
+
+def verifica_g1(mensagem) -> bool:
+    lista_g1 = ['últimas notícias do g1', 'ultimas noticias do g1',
+                'notícias g1', 'noticias g1', '!g1']
+    for palavra in lista_g1:
+        if palavra in mensagem:
+            return True
+    return False
+
+
+def verifica_uol(mensagem) -> bool:
+    lista_uol = ['últimas notícias do uol', 'ultimas noticias do uol',
+                 'notícias uol', 'noticias uol', '!uol']
+    for palavra in lista_uol:
+        if palavra in mensagem:
+            return True
+    return False
+
+
+def verifica_btc(mensagem):
+    lista_btc = ['bitcoin', '!btc']
+    for palavra in lista_btc:
+        if palavra in mensagem:
+            return True
+    return False
+
+
+def verifica_dolar(mensagem):
+    lista_btc = ['dolar', 'dólar', '!usd']
+    for palavra in lista_btc:
+        if palavra in mensagem:
+            return True
+    return False
+
 
 class MyClient(discord.Client):
+
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
         mensagem = message.content.lower()
-        match mensagem:
-            case '!help' | '!h' | 'help' | 'ajuda':
+        if message.author.name != 'AS':
+            if 'help' in mensagem or 'ajuda' in mensagem:
                 await message.channel.send(f'{message.author.name} os comandos são:{os.linesep}1 - (ultimas noticias g1?) ou (!g1): Mostra as últimas noticias do g1 {os.linesep}2 - (ultimas noticias uol?) ou (!uol): Mostra as últimas noticias do UOL{os.linesep}3 - (dolar?) ou (!USD): Mostra o valor atualizado do dólar{os.linesep}4 - (bitcoin?) ou (!BTC): Mostra o valor atualizado do bitcoin')
-            case 'ultimas noticias g1?' | '!g1':
+            elif saudacoes():
+                await message.channel.send('Olá, caso tenha dúvidas do que posso fazer, use o comando !help')
+            elif verifica_g1(mensagem):
                 await message.channel.send(ultimas_noticias_g1())
-            case 'ultimas noticias uol?' | '!uol':
+            elif verifica_uol(mensagem):
                 await message.channel.send(ultimas_noticias_uol())
-            case 'dolar?' | '!usd':
+            elif verifica_dolar(mensagem):
                 await message.channel.send(f'R$ {get_valor_dolar_atual()}')
-            case 'bitcoin?' | '!btc':
+            elif verifica_btc(mensagem):
                 await message.channel.send(f'R$ {get_valor_btc_atual()}')
+            else:
+                await message.channel.send('Desculpe, não entendi. Veja !help')
 
 
 intents = discord.Intents.default()
@@ -28,4 +74,5 @@ intents.message_content = True
 
 client = MyClient(intents=intents)
 client.run(
-    'MTA0MTg4MjUwNTAzODUzMjY3OQ.GKnLqT.MCqbtCLrdI-vXy1cLPALZtTIjeJB-AG0_ozl14')
+    'MTA0MTg4MjUwNTAzODUzMjY3OQ.GKnLqT.MCqbtCLrdI-vXy1cLPALZtTIjeJB-AG0_ozl14'
+)
